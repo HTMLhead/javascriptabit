00d02427074e87eae0f407234d65220566537c01
HTMLhead <mrdoo309@gmail.com>
2019-08-02 16:46:06 +0900

    solve: hello, getFirstValue

---
+++ Habitually/1.js
@@ -0,0 +1,5 @@
+function hello() {
+  return "hello edabit.com";
+}
+
+//https://edabit.com/challenge/ARr5tA458o2tC9FTN
---
+++ Habitually/2.js
@@ -0,0 +1,5 @@
+function getFirstValue(arr) {
+  return arr[0];
+}
+
+//https://edabit.com/challenge/QaApgtePE6QrCZ64o
