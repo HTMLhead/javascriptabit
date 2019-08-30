8fbd62a94f049fc1649d01ecb4b9dc0d034cb775
HTMLhead <mrdoo309@gmail.com>
2019-08-30 23:37:31 +0900

    solve: howManyTimes, checkAllEven

---
+++ Habitually/59.js
@@ -0,0 +1,7 @@
+function howManyTimes(num) {
+  let string = "";
+  for (let i = 0; i < num; i++) {
+    string += "a";
+  }
+  return "Ed" + string + "bit";
+}
---
+++ Habitually/60.js
@@ -0,0 +1,4 @@
+// Fix this incorrect code!
+function checkAllEven(arr) {
+  return arr.every(x => x % 2 === 0);
+}
