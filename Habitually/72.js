da901f47f42b9d82b39fa382299b261f6c8cfdc8
HTMLhead <mrdoo309@gmail.com>
2019-09-14 14:58:11 +0900

    solve: Go, mysteryFunc

---
+++ Habitually/72.js
@@ -0,0 +1,3 @@
+function mysteryFunc(arr, num) {
+  return arr.map(v => v % num);
+}
---
+++ Habitually/73.js
@@ -0,0 +1,7 @@
+function Go(num) {
+  let str = "";
+  for (let i = 0; i < num; i++) {
+    str += "-";
+  }
+  return str;
+}
