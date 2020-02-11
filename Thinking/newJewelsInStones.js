fcc501ff1e84e6270b3b675052a010687e21d932
HTMLhead <mrdoo309@gmail.com>
2020-02-11 20:44:25 +0900

    update: newJewlsInStones Set description

--- Thinking/newJewelsInStones.js
+++ Thinking/newJewelsInStones.js
@@ -16,3 +16,11 @@ var numJewelsInStones = function(J, S) {
   }
   return number;
 };
+
+const numJewelsInStones2 = (J, S) => {
+  const jewels = new Set(J);
+  return S.split("").reduce((res, s) => res + jewels.has(s), 0);
+};
+
+// Set 자료구조는 중복된 값을 제거해주니까 굳이 하나하나 더하며 찾을 필요가 없다.
+// hashmap 문제라고 판단되면 Set을 사용해보자.
